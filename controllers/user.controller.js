9fa7d85e4725952531459abcdfeaace9adb0cd9e
Krauchelli <mekaku26shi@gmail.com>
2024-06-06 21:54:37 +0700

    Update user.controller.js to use relative paths for rendering views

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,19 +8,20 @@ const prisma = new PrismaClient();
 // get all user
 const getAllUser = async (req, res) => {
     const users = await prisma.user.findMany();
-    res.render('users/profile', { users });
+    res.render('profile', { users });
 };
 
 // get specific user, bisa dipake untuk profile routing
 const getUser = async (req, res) => {
-    const { id } = req.params;
+    const { username } = req.params;
     const user = await prisma.user.findUnique({
         where: {
-            id: parseInt(id),
+            username
         },
     });
+    console.log(user);
 
-    res.render('users/profile', { user });
+    res.render('profile', { user });
 };  
 
 module.exports = {
