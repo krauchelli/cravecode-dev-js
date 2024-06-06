859e743bbbafbdbda5b9c0023eee9832e871c18a
Krauchelli <mekaku26shi@gmail.com>
2024-06-06 21:54:24 +0700

    Add registerController to handle user registration

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,8 +1,10 @@
 const { getAllUser, getUser } = require('./user.controller');
+const { registerController } = require('./auth.controller');
 
 const controllers = {
     getAllUser,
     getUser,
+    registerController
 };
 
 module.exports = controllers;
