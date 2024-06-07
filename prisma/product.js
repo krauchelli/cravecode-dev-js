c1243b694318efc05151ee9ba206223eea0f440f
Krauchelli <mekaku26shi@gmail.com>
2024-06-07 14:36:42 +0700

    initial commit

---
+++ prisma/product.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+      name: "1-code chocolate megabit",
+      price: 12000.00
+    },
+    {
+      name: "box-code bit",
+      price: 32000.00
+    },
+    {
+      name: "4-code premium bit",
+      price: 56000.00
+    },
+    {
+      name: "5-code mix megabit",
+      price: 100000.00
+    }
+  ];
+
+module.exports = products;
