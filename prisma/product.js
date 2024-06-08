25cc2b2e17b375108d0de501756ac3db1bbe8e1d
Krauchelli <mekaku26shi@gmail.com>
2024-06-08 22:19:01 +0700

    feat: Add product images and update product data

--- prisma/product.js
+++ prisma/product.js
@@ -1,19 +1,39 @@
 const products = [
     {
+        id: "product1",
       name: "1-code chocolate megabit",
-      price: 12000.00
+      price: 12000.00,
+      imagePath: "images/product1.png"
     },
     {
+        id: "product2",
       name: "box-code bit",
-      price: 32000.00
+      price: 32000.00,
+      imagePath: "images/product2.png"
     },
     {
+        id: "product3",
       name: "4-code premium bit",
-      price: 56000.00
+      price: 56000.00,
+      imagePath: "images/product3.png"
     },
     {
+        id: "product4",
       name: "5-code mix megabit",
-      price: 100000.00
+      price: 60000.00,
+      imagePath: "images/product4.png"
+    },
+    {
+        id: "product5",
+      name: "birth-code gigabit",
+      price: 39000.00,
+      imagePath: "images/product5.png"
+    },
+    {
+        id: "product6",
+      name: "all-code gift hampers",
+      price: 84000.00,
+      imagePath: "images/product6.png"
     }
   ];
 
