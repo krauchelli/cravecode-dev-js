15dbff8b1735defd0f1490051242986d8fa74126
Krauchelli <mekaku26shi@gmail.com>
2024-06-09 18:26:55 +0700

    feat: Add payMethod module for supported payment methods

---
+++ prisma/paymethod.js
@@ -0,0 +1,28 @@
+const payMethod = [
+    {
+        id: "method1",
+        name: "GOPAY"
+    },
+    {
+        id: "method2",
+        name: "OVO"
+    },
+    {
+        id: "method3",
+        name: "DANA"
+    },
+    {
+        id: "method4",
+        name: "Link Aja"
+    },
+    {
+        id: "method5",
+        name: "Shopee Pay"
+    },
+    {
+        id: "method6",
+        name: "Transfer Bank"
+    }
+]
+
+module.exports = payMethod;
