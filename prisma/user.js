7ae6b8a91374eb6039836931aedd68e446e0d54f
Krauchelli <mekaku26shi@gmail.com>
2024-06-10 20:16:07 +0700

    feat: Hash user passwords using bcrypt

--- prisma/user.js
+++ prisma/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 //collection of random user data
 const user = [
     {
@@ -15,27 +17,37 @@ const user = [
     {
         username: 'yama',
         email: 'user1@example.com',
-        password: 'user1123',
+        password: 'yama123',
         phoneNumber: '1111111111'
     },
     {
         username: 'kotoha',
         email: 'user2@example.com',
-        password: 'user2123',
+        password: 'kotoha123',
         phoneNumber: '2222222222'
     },
     {
         username: 'dini',
         email: 'user3@example.com',
-        password: 'user3123',
+        password: 'dini123',
         phoneNumber: '3333333333'
     },
     {
         username: 'ladya',
         email: 'user4@example.com',
-        password: 'user4123',
+        password: 'ladya123',
         phoneNumber: '4444444444'
     }
 ];
+
+// hash passwords using bcrypt
+
+user.forEach((user) => {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(user.password, salt);
+    user.password = hashedPassword;
+});
+
+module.exports = user;
 
 module.exports = user;
