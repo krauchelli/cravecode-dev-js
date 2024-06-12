98868c211992c915a8ad7db2a43c937530512c72
Krauchelli <mekaku26shi@gmail.com>
2024-06-12 09:50:23 +0700

    feat: Add checkUserRole middleware for admin authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,6 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const auth = async (req, res, next) => {
     try {
         if (!req.session.user) {
@@ -28,4 +31,27 @@ const redirectIfAuthenticated = async (req, res, next) => {
     }
 };
 
- module.exports = { auth, redirectIfAuthenticated };
+const checkUserRole = async (req, res, next) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.session.user
+            },
+        });
+
+        if (!user) {
+            return res.status(401).json({ message: 'invalid user' });
+        }
+
+        // pengecekan apakah user adalah admin
+        if (user.role !== 'ADMIN') {
+            // kalau bukan admin, redirect ke halaman home
+            return res.redirect('/profile');
+        }
+        next(); // Call the next middleware function
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+ module.exports = { auth, redirectIfAuthenticated, checkUserRole };
