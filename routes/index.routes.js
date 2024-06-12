60200bc133c9544be24dc730fba620e9d3ca7cab
Krauchelli <mekaku26shi@gmail.com>
2024-06-12 09:50:39 +0700

    refactor: Update index.routes.js to include admin dashboard routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {auth, redirectIfAuthenticated} = require('../middlewares/auth');
+const { auth, redirectIfAuthenticated, checkUserRole } = require('../middlewares/auth');
 
 const controllers = require('../controllers/index.controller');
 
@@ -15,12 +15,27 @@ router.get('/login', redirectIfAuthenticated, async (req, res) => {
 router.get('/register', redirectIfAuthenticated, async (req, res) => {
     res.render('register');
 });
+router.get('/dashboard', auth, checkUserRole, async (req, res) => {
+    res.render('dashboard');
+});
 router.get('/profile', auth, controllers.getUser);
 router.get('/shop', auth, controllers.getAllCart);
 router.get('/cart', auth, controllers.getAllCart); // not finished
 router.get('/inprocess', auth, controllers.getAllProcess); // not finished
 router.get('/completed', auth, controllers.getAllCompleted); // not finished
 // router.get('/orderList', auth, controllers.getAllOrder); // not finished
+
+// penggunaan untuk admin
+router.get('/dashboard/user', auth, checkUserRole, controllers.showAllUser);
+router.get('/dashboard/user/:id', auth, checkUserRole, controllers.showUser);
+router.get('/dashboard/cart', auth, checkUserRole, controllers.showAllCart);
+router.get('/dashboard/cart/:id', auth, checkUserRole, controllers.showCart);
+router.get('/dashboard/order', auth, checkUserRole, controllers.showAllOrder);
+router.get('/dashboard/order/:id', auth, checkUserRole, controllers.showOrder);
+router.get('/dashboard/product', auth, checkUserRole, controllers.showAllProduct);
+router.get('/dashboard/product/:id', auth, checkUserRole, controllers.showProduct);
+router.get('/dashboard/paymethod', auth, checkUserRole, controllers.showAllPayMethod);
+router.get('/dashboard/paymethod/:id', auth, checkUserRole, controllers.showPayMethod);
 
 // kumpulan post
 router.post('/register', controllers.registerController);
