19dcecac1f8cdd2dfac34003921b2a8fdb084ff5
Krauchelli <mekaku26shi@gmail.com>
2024-06-16 18:35:26 +0700

    feat: Update session configuration with MemoryStore

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const router = require('./routes/index.routes');
 const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const dotenv = require('dotenv');
 
 const app = express();
@@ -17,6 +18,10 @@ app.use(express.urlencoded({ extended: true })); // jika mengirimkan data dalam 
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public'))); // static files dari folder public
 app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
     secret: process.env.SESSION_SECRET,
     resave: process.env.SESSION_RESAVE,
     saveUninitialized: process.env.SESSION_SAVE_UNINITIALIZED,
